e78f3b8666beb94950e03f0a38644378e315a1f9
leon <liyang@hosecloud.com>
2020-10-22 20:37:33 +0800

    feat: jest的使用

---
+++ test/json.test.js
@@ -0,0 +1,12 @@
+/**
+ * @description 
+ * @author Leon
+ */
+
+const server = require('./server')
+test('json 接口返回数据正确', async () => {
+  const res = await server.get('/json')
+  expect(res.body).toEqual({
+    title: 'koa2 json'
+  })
+})
