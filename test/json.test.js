21b4da3cadbe9c265b21ae129b4762a7e0433aae
liyang <leonvincent94@outlook.com>
2020-10-25 11:46:10 +0800

    feat: init

--- test/json.test.js
+++ test/json.test.js
@@ -4,13 +4,22 @@
  */
 
 const server = require('./server')
-test('json 接口返回数据正确', async () => {
-  const res = await server.get('/json')
-  // const res1 = await server.post('/login').send({
-  //   userName: 'zhangsan',
-  //   password: '123'
-  // })
-  expect(res.body).toEqual({
-    title: 'koa2 json'
-  })
+// test('json 接口返回数据正确', async () => {
+//   const res = await server.get('/json')
+//   // const res1 = await server.post('/login').send({
+//   //   userName: 'zhangsan',
+//   //   password: '123'
+//   // })
+//   expect(res.body).toEqual({
+//     title: 'koa2 json'
+//   })
+// })
+
+function sum(a, b) {
+  return a + b
+}
+
+test('10 + 20 = 20', () => {
+  const res = sum(10, 20)
+  expect(res).toBe(30)
 })
